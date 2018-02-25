f23db0602732e996504a45668355628212c59d28
Christian Fecteau <christian.fecteau@gmail.com>
2018-02-25 17:27:56 -0500

    fix name

--- build/package.sh
+++ build/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-convert src/icon.png -resize 16x16  chrome-extension/icons/icons-16.png
-convert src/icon.png -resize 48x48  chrome-extension/icons/icons-48.png
-convert src/icon.png -resize 128x128  chrome-extension/icons/icons-128.png
+convert chrome-webstore/icon.png -resize 16x16  chrome-extension/icons/icon-16.png
+convert chrome-webstore/icon.png -resize 48x48  chrome-extension/icons/icon-48.png
+convert chrome-webstore/icon.png -resize 128x128  chrome-extension/icons/icon-128.png
 
 cp LICENSE chrome-extension
 zip -r chrome-extension chrome-extension -x \*\/.DS_Store
